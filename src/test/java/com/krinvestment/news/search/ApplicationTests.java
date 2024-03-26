d005620fc61608e3a29c52b820d1f83a5bd58bd3
csh1man <tjgusyt@gmail.com>
2024-03-26 11:13:24 +0900

    first commit

---
+++ src/test/java/com/krinvestment/news/search/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krinvestment.news.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
